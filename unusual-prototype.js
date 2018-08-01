e9c54277af1dff67b5a91315afa6500647603517
fsh-kraken <fsh-kraken@users.noreply.github.com>
2018-08-01 13:45:21 +0800

    Add files via upload

---
+++ unusual-prototype.js
@@ -0,0 +1,11 @@
+String.prototype.toMoney = function(decimal,separator){
+    var decimal = decimal == undefined ? 2 : decimal;
+    var separator = separator == undefined ? ',' : separator;
+    var number = parseFloat(this).toFixed(decimal);
+    number = number.toString().replace(/\,/g,''); /** remove commas to clean the string */
+    /** split reverse and join then divide the string into 3 parts then insert the separator you wish for */
+    return number.split('').reverse().join('').replace(/(\d{3}(?!.*\.|$))/g, '$1'+separator).split('').reverse().join('');
+}
+
+var x = "100000000000";
+console.log(x.toMoney(0));
